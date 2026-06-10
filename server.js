74266fe84388639837ff7455fd3c0ad7689f71d3
thomasjstone-create <thomas.j.stone@gmail.com>
2026-06-10 14:59:32 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -119,6 +119,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
